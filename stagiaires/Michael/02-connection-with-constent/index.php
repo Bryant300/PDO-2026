d0903f6571d118ea68cbbea77dcecbdafc30b4fc
WebDevCF2m <gitweb@cf2m.be>
2026-04-24 12:15:39 +0200

    Create my First fetchAll

--- stagiaires/Michael/02-connection-with-constent/index.php
+++ stagiaires/Michael/02-connection-with-constent/index.php
@@ -20,7 +20,7 @@ try{
                     // ;charset=utf8mb4;
                     ";charset=".DB_CONNECT_CHARSET . ";"
         ,
-        username:   "",
+        username:   DB_CONNECT_USER,
         password:   "",
         );
 
@@ -32,3 +32,77 @@ try{
     echo "<br>Message de l'Erreur : ".$e->getMessage(); // le message
 
 }
+
+// création de la requête SELECT sans variables utilisateurs
+// du plus peuplé au moins peuplé
+$sql = "
+    SELECT c.`nom`, c.`population`, c.`continent`, c.`capitale` 
+        FROM `countries` c 
+        ORDER BY c.`population` DESC;
+        ";
+
+// véritable requête
+$request = $db->query($sql);
+
+// on récupère les données dans un format lisible par PHP
+$resultats = $request->fetchAll(PDO::FETCH_ASSOC);
+
+// bonne pratique
+// pas utile pour MariaDB ou MySQL
+$request->closeCursor();
+
+// bonne pratique
+// pas utile pour MariaDB ou MySQL
+// sauf si requête premanente 
+$db = null;
+
+/*
+// transformation en json
+$json = json_encode($resultats, JSON_PRETTY_PRINT);
+
+// on va écrire un fichier .json
+// pour ne plus devoir faire la requête
+// pour monsieur tout le monde
+// création du fichier si il n'existe pas, sinon ouverture
+$file = fopen("allCountries-".date('YmdHis')."-". uniqid(true). ".json","wr");
+
+    fputs($file,$json);
+fclose($file);
+*/
+
+//var_dump($db,$request,$resultats);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les pays du monde</title>
+</head>
+<body>
+    <h1>Les pays du monde</h1>
+     <table>
+        <thead>
+            <tr>
+                <td>Pays</td>
+                <td>Population</td>
+                <td>Capitale</td>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        // tant qu'on a des pays
+        foreach($resultats as $item):
+        ?>
+        <tr>
+            <td><span title="<?= $item['continent'] ?>"><?= $item['nom'] ?></span></td>
+            <td><?= $item['population'] ?></td>
+            <td><?= $item['capitale'] ?></td>
+        </tr>
+        <?php
+        endforeach;
+        ?>
+        </tbody>
+    </table>
+</body>
+</html>
